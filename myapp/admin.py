fad788931942b9e5a2b959104355fa0a6bc4f434
Usman Sadiq <usmansadiq.cs@gmail.com>
2022-04-30 21:13:54 +0500

    Django image uploader project

---
+++ myapp/admin.py
@@ -0,0 +1,7 @@
+from csv import list_dialects
+from django.contrib import admin
+from .models import Image
+# Register your models here.
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['id','photo','date'] 
